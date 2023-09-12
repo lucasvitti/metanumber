b23a54e28a565e8b35f78770f67f4b56bb3b32bd
Lucas Vitti <84133691+lucasvitti@users.noreply.github.com>
2023-09-11 21:58:12 -0300

    Add files via upload

--- MetaNumber.py
+++ MetaNumber.py
@@ -215,7 +215,7 @@ class MetaNumber:
             r = q % base
             q = q//base
             if verbose:
-                print("{} = {} x {} + {}".format(q,q_old,base,r))
+                print("{} = {} x {} + {}".format(q_old,q,base,r))
     
             if r < 0:
                 q += 1
